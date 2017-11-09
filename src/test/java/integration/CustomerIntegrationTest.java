dace0dc871224ae6f0c317f5660a7912bc360ede
harrimit <mitchell.harris@asynchrony.con>
2017-11-09 15:33:52 -0600

    fixing up the integration tests - Mitch & Jim

--- src/test/java/integration/CustomerIntegrationTest.java
+++ src/test/java/integration/CustomerIntegrationTest.java
@@ -1,10 +1,16 @@
 package integration;
 
+import io.restassured.response.ValidatableResponse;
 import objects.Customer;
 import org.junit.Test;
+
+import java.net.URL;
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class CustomerIntegrationTest {
 
@@ -13,8 +19,24 @@ public class CustomerIntegrationTest {
     public void testPostStuff_noParam() {
         Customer customer = new Customer("bob", "saget");
 
-        given().body(customer)
-            .when().post("/springThing/stuff")
-            .then().body("id", notNullValue());
+        given().contentType("application/json").body(customer)
+            .when().post("/stuff")
+            .then().body("id", greaterThan(0))
+            .body("firstName",is("bob"))
+            .body("lastName", is("saget"));
+    }
+
+    @Test
+    public void testPostStuff_checkingQeueryParam() {
+        Customer customer = new Customer("sam", "Iam");
+
+        given().contentType("application/json").body(customer)
+                .when().post("/stuff")
+                .then().body("id", notNullValue());
+
+        ValidatableResponse response = given().queryParam("firstName", "sam")
+                .when().get("/stuff")
+                .then().body("size()", greaterThan(0));
+
     }
 }
